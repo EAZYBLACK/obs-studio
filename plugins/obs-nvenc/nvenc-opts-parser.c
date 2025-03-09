b51149e6e443b4c8482803898ef9a04c107cb393
EAZYBLACK <eazyblack40@gmail.com>
2025-03-09 13:39:51 +0200

    Revert "obs-nvenc: Fix building with nvcodec 12.2"
    This reverts commit c7b0c63d01a447224cd6e3d141de30e28a92512d.

--- plugins/obs-nvenc/nvenc-opts-parser.c
+++ plugins/obs-nvenc/nvenc-opts-parser.c
@@ -79,7 +79,7 @@ static bool apply_rc_opt(const struct obs_option *opt, NV_ENC_RC_PARAMS *nv_conf
 	APPLY_BIT_OPT(aqStrength, 4)
 
 #ifdef NVENC_12_2_OR_LATER
-	APPLY_INT_OPT(lookaheadLevel, NV_ENC_LOOKAHEAD_LEVEL, PRIu32)
+	APPLY_INT_OPT(lookaheadLevel, NV_ENC_LOOKAHEAD_LEVEL)
 #endif
 
 	/* Macros above will return true if succesfully evaluated.
@@ -146,7 +146,7 @@ static bool apply_hevc_opt(struct obs_option *opt, NV_ENC_CONFIG_HEVC *nv_conf)
 	APPLY_INT_OPT(idrPeriod, uint32_t, PRIu32)
 	APPLY_INT_OPT(useBFramesAsRef, NV_ENC_BFRAME_REF_MODE, PRIu32)
 #ifdef NVENC_12_2_OR_LATER
-	APPLY_INT_OPT(tfLevel, NV_ENC_TEMPORAL_FILTER_LEVEL, PRIu32)
+	APPLY_INT_OPT(tfLevel, NV_ENC_TEMPORAL_FILTER_LEVEL)
 #endif
 
 	APPLY_BIT_OPT(enableFillerDataInsertion, 1)
