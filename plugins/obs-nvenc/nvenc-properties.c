9585ad10061f36a6e64703fdeee7bbe49bbd52eb
EAZYBLACK <eazyblack40@gmail.com>
2025-03-09 13:38:58 +0200

    Revert "obs-nvenc: Only show UHQ tune on supported GPUs"
    This reverts commit db5b226bb93e7478e76e3e92a21f9f29dfbb89cc.

--- plugins/obs-nvenc/nvenc-properties.c
+++ plugins/obs-nvenc/nvenc-properties.c
@@ -157,10 +157,7 @@ obs_properties_t *nvenc_properties_internal(enum codec_type codec)
 
 #define add_tune(val) obs_property_list_add_string(p, obs_module_text("Tuning." val), val)
 #ifdef NVENC_12_2_OR_LATER
-	/* The UHQ tune is only supported on Turing or later.
-	 * It uses the temporal filtering feature, so we can use its
-	 * availability as an indicator that we are on a supported GPU. */
-	if (codec == CODEC_HEVC && caps->temporal_filter)
+	if (codec == CODEC_HEVC)
 		add_tune("uhq");
 #endif
 	add_tune("hq");
